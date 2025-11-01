b94be418898d6bc7554a1b3b568cc15aac5d5572
sharksback <mrsharksback@tamu.edu>
2025-11-01 15:52:59 -0500

    Nothing important

--- pos-frontend/src/pages/Menu.jsx
+++ pos-frontend/src/pages/Menu.jsx
@@ -2,6 +2,8 @@ export default function Menu() {
   return (
     <main>
       <h1>Customer Menu (Placeholder)</h1>
+      <button>Drink 1</button>
+      <button>Drink 2</button>
     </main>
   );
 }
