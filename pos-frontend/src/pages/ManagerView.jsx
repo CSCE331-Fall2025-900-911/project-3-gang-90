951932d175a6ece8295ce0f6a6958b5064cd32f2
balar0 <144611586+balar0@users.noreply.github.com>
2025-11-08 17:03:11 -0600

    removed unneeded functions

--- pos-frontend/src/pages/ManagerView.jsx
+++ pos-frontend/src/pages/ManagerView.jsx
@@ -1,7 +1,7 @@
-import {getEmployee, getMenu} from '../fetchData.js';
+
 
 export default  function ManagerView() {
-  console.log( getEmployee());
+  
 
   return <h1>Manager view (Placeholder)</h1>;
 }
