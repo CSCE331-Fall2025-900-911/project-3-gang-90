f5092bfd93e26f8b45a114fe3e5f42990962b73c
balar0 <144611586+balar0@users.noreply.github.com>
2025-11-19 16:32:31 -0600

    added MuI

---
+++ pos-frontend/src/components/MangerComponets/MangerTopBar.jsx
@@ -0,0 +1,25 @@
+
+
+
+
+export default function MangerTopBar({pageName}){
+
+
+    return(
+        <div className="mangerTopBar">
+            <div >
+                <h1>
+                {pageName}
+                </h1>
+            </div>
+            <div className="mangerRightAlign">
+                add time
+            </div>
+
+        </div>
+
+
+    )
+
+
+}
--- pos-frontend/src/components/MangerComponets/SalesReport.jsx
+++ pos-frontend/src/components/MangerComponets/SalesReport.jsx
@@ -1,20 +1,62 @@
+import Button from "@mui/material/Button";
 import MangerPage from "./MangerPage"
-
-
+import { BarChart } from '@mui/x-charts/BarChart';
+import { DataGrid } from '@mui/x-data-grid';
+import Paper from '@mui/material/Paper';
 
 
 
 export default function SalesReport(){
 
 
+
+
     return(
-        <MangerPage  child={
+        <MangerPage  
+        pageName={"Sales Report"}
+        child={
             <div>
                  <h1>hi</h1>
                 <p> 
                     my name is Brendan
                 </p>
-
+                <div>
+                    Start date:
+                    <input type="date"></input>
+                    end date:
+                    <input type="date"></input>
+                    
+                </div>
+                <div>
+                    <Button >Today</Button>
+                    <Button>Last 7 days</Button>
+                    <Button>Last 30 days</Button>
+                    <Button>This month</Button>
+                </div>
+                <div>
+                    <BarChart
+                        xAxis={[
+                            {
+                            id: 'barCategories',
+                            data: ['bar A', 'bar B', 'bar C'],
+                            },
+                        ]}
+                        series={[
+                            {
+                            data: [2, 5, 3],
+                            },
+                        ]}
+                        height={300}
+                    />
+                </div>
+                <div>
+                    <table>
+                        <tr>
+                            <td>Item</td>
+                            <td>Time</td>
+                        </tr>
+                    </table>
+                </div>
             </div>
         }/>
     );
