dda28b45fb0fcca2b4abae69adadf8ed15c623d9
balar0 <144611586+balar0@users.noreply.github.com>
2025-11-20 00:04:40 -0600

    added router code for sales report

--- pos-frontend/src/components/MangerComponets/MangerSideBar.jsx
+++ pos-frontend/src/components/MangerComponets/MangerSideBar.jsx
@@ -9,16 +9,28 @@ export default function MangerSideBar(){
     return(
         <div className='mangerSideBar' >
             <div className='mangerLinkPadding'>
-                <Link className='mangerLink'>sales report</Link>
+                <Link className='mangerLink' to="/cashier">Chashier</Link>
             </div>
             <div className='mangerLinkPadding'>
-                <Link className='mangerLink'>cashire</Link>
+                <Link className='mangerLink' to="/transactions">Transactions</Link>
             </div>
             <div className='mangerLinkPadding'>
-                <Link className='mangerLink'></Link>
+                <Link className='mangerLink' to="/products">Productst</Link>
             </div>
             <div className='mangerLinkPadding'>
-                <Link className='mangerLink'></Link>
+                <Link className='mangerLink' to="/employees">Employees</Link>
+            </div>
+            <div className='mangerLinkPadding'>
+                <Link className='mangerLink' to="/xReport">X-Report</Link>
+            </div>
+            <div className='mangerLinkPadding'>
+                <Link className='mangerLink'to={"/usageChart"}>Usage Chart</Link>
+            </div>
+            <div className='mangerLinkPadding'>
+                <Link className='mangerLink' to={"/salesReport"}>Sales Report</Link>
+            </div>
+            <div className='mangerLinkPadding'>
+                <Link to={"/reportz"} className='mangerLink'>Z-Report</Link>
             </div>
             
         </div>
--- pos-frontend/src/components/MangerComponets/SalesReport.jsx
+++ pos-frontend/src/components/MangerComponets/SalesReport.jsx
@@ -3,35 +3,88 @@ import MangerPage from "./MangerPage"
 import { BarChart } from '@mui/x-charts/BarChart';
 import { DataGrid } from '@mui/x-data-grid';
 import Paper from '@mui/material/Paper';
+import { useEffect } from "react";
 
 
 
 export default function SalesReport(){
 
+    const columns = [
+  { field: 'id', headerName: 'ID', width: 100 },
+  { field: 'firstName', headerName: 'First name', width: 230 },
+  { field: 'lastName', headerName: 'Last name', width: 230 },
+  {
+    field: 'age',
+    headerName: 'Age',
+    type: 'number',
+    width: 100,
+  },
+  {
+    field: 'fullName',
+    headerName: 'Full name',
+    description: 'This column has a value getter and is not sortable.',
+    sortable: false,
+    width: 260,
+    valueGetter: (value, row) => `${row.firstName || ''} ${row.lastName || ''}`,
+  },
+];
+
+const paginationModel = { page: 0, pageSize: 5 };
+const rows = [
+  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+];
 
 
+
+    // useEffect({
+
+
+
+
+
+    // },[]);
 
     return(
         <MangerPage  
         pageName={"Sales Report"}
         child={
             <div>
-                 <h1>hi</h1>
-                <p> 
-                    my name is Brendan
-                </p>
-                <div>
-                    Start date:
-                    <input type="date"></input>
-                    end date:
-                    <input type="date"></input>
+                <div className="flex p-5">
+                    <div className="p-5 flex">
+                        <p className="p-10">
+                        Start date:
+                        </p>
+                        <input type="date"></input>
+                    </div>
+                    <div className="p-5 flex">
+                        <p className="p-10 ">
+                        End date:
+                        </p>
+                        <input  type="date"></input>
+                    </div>
                     
                 </div>
-                <div>
-                    <Button >Today</Button>
-                    <Button>Last 7 days</Button>
-                    <Button>Last 30 days</Button>
-                    <Button>This month</Button>
+                <div className="flex">
+                    <div className="p-2">
+                        <Button>Today</Button>
+                    </div>
+                    <div className="p-2">
+                        <Button>Last 7 days</Button>
+                    </div>
+                    <div className="p-2">
+                        <Button>Last 30 days</Button>
+                    </div>
+                    <div className="p-2"> 
+                        <Button>This month</Button>
+                    </div>
                 </div>
                 <div>
                     <BarChart
@@ -46,16 +99,21 @@ export default function SalesReport(){
                             data: [2, 5, 3],
                             },
                         ]}
-                        height={300}
+                        // height={400}
+                        // width={1000}
+                        sx={{height: 400, width: '100%'}}
                     />
                 </div>
                 <div>
-                    <table>
-                        <tr>
-                            <td>Item</td>
-                            <td>Time</td>
-                        </tr>
-                    </table>
+                    <Paper sx={{ height: 400, width: '100%' }}>
+                        <DataGrid
+                            rows={rows}
+                            columns={columns}
+                            initialState={{ pagination: { paginationModel } }}
+                            pageSizeOptions={[5, 10]}
+                            sx={{ border: 0 }}
+                         />
+                     </Paper>
                 </div>
             </div>
         }/>
