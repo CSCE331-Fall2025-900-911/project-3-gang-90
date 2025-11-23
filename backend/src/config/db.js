4d7ba6fc0ddc92c4ee5be61eb8b28a19c32e7b69
Daniel Zhang <zhangdaniel62@gmail.com>
2025-11-23 02:54:42 -0600

    argh

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,4 +1,12 @@
 import postgres from "postgres";
 import { DATABASE_URL } from "./index.js";
 
-export const sql = postgres(DATABASE_URL);
+if (!DATABASE_URL) {
+    throw new Error("DATABASE_URL is not defined!");
+}
+
+
+export const sql = postgres(DATABASE_URL, {
+    ssl: "require",
+    max: 10
+});
