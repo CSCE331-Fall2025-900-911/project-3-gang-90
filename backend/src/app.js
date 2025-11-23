9057233a3d07177f13ed9a0e91abb0e86a77962a
Daniel Zhang <zhangdaniel62@gmail.com>
2025-11-23 16:26:11 -0600

    Made changes to api and config

--- backend/src/app.js
+++ backend/src/app.js
@@ -31,7 +31,7 @@ app.use('/auth/google', authenticationRouter)
 app.use(errorHandler);
 
 app.get('/', (req, res) => {
-    res.status(200).send('IM RUNNING FINE BITCH FUCK YOU LOOKING AT');
+    res.status(200).send('Backend works!');
 })
 
 export default app
