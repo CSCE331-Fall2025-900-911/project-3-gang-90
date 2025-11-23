b6cf7ff39bb949b5d75e9d9ae7aad57c759c4aa6
Kevin Chen <kchen2561@gmail.com>
2025-11-22 20:30:00 -0600

    fix issue with drinks not loading

--- pos-frontend/src/App.jsx
+++ pos-frontend/src/App.jsx
@@ -29,6 +29,7 @@ import LoginTest from './components/LoginButton'
 import ManagerProducts from "./Project2/ManagerProducts"
 import SalesReport from './components/MangerComponets/SalesReport'
 import ZReport from './Project2/ZReport'
+import XReport from './Project2/XReport'
 
 export default function App() {
   
@@ -48,6 +49,7 @@ export default function App() {
             <Route path="/cart" element={<Cart />} />
             <Route path="/edit/:id" element={<EditItem />} />
             <Route path="/settings" element={<Settings />} />
+            <Route path="/reportx" element={<XReport />} />
             <Route path="/reportz" element ={<ZReport/>} />
             <Route path="/products" element={<ManagerProducts />} />
             <Route path="/salesReport" element={<SalesReport/>}/>
--- pos-frontend/src/Project2/XReport.jsx
+++ pos-frontend/src/Project2/XReport.jsx
@@ -14,13 +14,21 @@ child={
           <p className='font-bold text-3xl pb-5'>X Report</p>
           <div className='flex flex-row w-1/2 items-center gap-7 pb-5'>
             <button onClick={()=>setRefreshKey(k=>k+1)} className='border-2 p-1 rounded-lg bg-gray-200 hover:bg-gray-300 cursor-pointer'>Refresh</button>
-            {/* havent implemented the reset logic yet */}
-            <button className='border-2 p-1 rounded-lg bg-gray-200 hover:bg-gray-300 cursor-pointer'>Finalize (Reset)</button>
           </div>
-          <p className='font-bold'>Total Sales:</p>
-          {/* change here */}
-          <p className='text-3xl pb-5'>$0.00</p>
-          {/* ----------- */}
+          <div className='flex flex-row gap-4 w-full'>
+            <div>
+              <p className='font-bold'>Total Sales:</p>
+              {/* change here */}
+              <p className='text-3xl pb-5'>$0.00</p>
+              {/* ----------- */}
+            </div>
+            <div>
+              <p className='font-bold'>Total Transactions:</p>
+              {/* change herer */}
+              <p className='text-3xl pb-5'>0</p>
+              {/* ------------ */}
+            </div>
+          </div>
           <p className='font-bold'>Per-Employee Summary:</p>
 
           <div className='flex-none h-[480px] overflow-auto rounded-md border mb-5 border-gray-300 bg-white p-4 shadow-sm'>
