11f5336b4731d4215d65043b230debb3fc9d650c
Daniel Zhang <zhangdaniel62@gmail.com>
2025-11-24 21:00:31 -0600

    Started the addition of testing for backend's api endpoints

--- backend/src/tests/dbTest.js
+++ backend/src/tests/dbTest.js
@@ -1,16 +1,37 @@
-import { sql } from "../config/db.js";
+import test from "node:test"
+import assert from "node:assert"
+import { sql } from "../config/db.js"
 
-async function testSSL() {
-    try {
-        const [row] = await sql`SHOW ssl;`;
-        console.log("ssl:", row);
-    }
-    catch (err) {
-        console.error(err);
-    } 
-    finally {
-        sql.end();
-    }
-}
 
-testSSL();
+test("Checking database connection", async () => {
+    const res = await sql`SELECT 1;`;
+    assert.strictEqual(res[0], 1);
+});
+
+test("Checking existence of table: ingredients", async () => {
+    // TODO: Implement later
+});
+
+test("Checking existence of table: ingredients_map", async () => {
+    // TODO: Implement later
+});
+
+test("Checking existence of table: menu", async () => {
+    // TODO: Implement later
+});
+
+test("Checking existence of table: personnel", async () => {
+    // TODO: Implement later
+});
+
+test("Checking existence of table: seasonal_menu", async () => {
+    // TODO: Implement later
+});
+
+test("Checking existence of table: transaction_details", async () => {
+    // TODO: Implement later
+});
+
+test("Checking existence of table: transactions", async () => {
+    // TODO: Implement later
+});
