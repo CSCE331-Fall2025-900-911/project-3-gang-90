e0ef1860520c838c6a1b499d20a62cfa7462aa50
balar0 <144611586+balar0@users.noreply.github.com>
2025-12-06 00:15:35 -0600

    added google translation

--- pos-frontend/src/App.jsx
+++ pos-frontend/src/App.jsx
@@ -33,6 +33,7 @@ import SalesReport from './components/MangerComponets/SalesReport'
 import ZReport from './Project2/ZReport'
 import XReport from './Project2/XReport'
 import UsageReport from './components/MangerComponets/UsageReport'
+import { useEffect } from 'react'
 
 export default function App() {
   
