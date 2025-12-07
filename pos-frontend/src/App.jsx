3443b9d0c944bd369f66829a9e4ea401092c7b89
Kevin Chen <kchen2561@gmail.com>
2025-12-07 14:20:33 -0600

    merge conflict

--- pos-frontend/src/App.jsx
+++ pos-frontend/src/App.jsx
@@ -35,6 +35,7 @@ import XReport from './Project2/XReport'
 import UsageReport from './components/MangerComponets/UsageReport'
 import { useEffect } from 'react'
 import Transactions from './components/MangerComponets/Transaction';
+import LoginTest from './components/LoginTest'
 
 export default function App() {
   
