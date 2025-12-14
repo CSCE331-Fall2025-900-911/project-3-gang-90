0a54c7dc4df902f5e30fece7a8e6ab2b3a2dc2a1
balar0 <144611586+balar0@users.noreply.github.com>
2025-12-13 22:55:37 -0600

    fixed mrege conflict error

--- pos-frontend/src/Project2/ZReport.jsx
+++ pos-frontend/src/Project2/ZReport.jsx
@@ -45,24 +45,12 @@ const ZReport = () => {
 <MangerPage 
 pageName={"Z-Report"}
 child={
-  <div>
       <div className='flex-1 flex min-h-0 flex-col overflow-hidden'>
           <p className='font-bold text-3xl pb-5'>Z Report</p>
           <div className='flex flex-row w-1/2 items-center gap-7 pb-5'>
             <button onClick={()=>setRefreshKey(k=>k+1)} className='border-2 p-1 rounded-lg bg-gray-200 hover:bg-gray-300 cursor-pointer'>Refresh</button>
-            {/* havent implemented the reset logic yet */}
-            <button className='border-2 p-1 rounded-lg bg-gray-200 hover:bg-gray-300 cursor-pointer'>Finalize (Reset)</button>
-          </div>
-          <p className='font-bold'>Total Sales:</p>
-          {/* change here */}
-          <p className='text-3xl pb-5'>$0.00</p>
-          {/* ----------- */}
-          <p className='font-bold'>Per-Employee Summary:</p>
-          <div className='flex-none h-[480px] overflow-auto rounded-md border mb-5 border-gray-300 bg-white p-4 shadow-sm'>
-            <ReportTable refreshSignal={refreshKey} />
-          </div>
             <button onClick={() => { setFinalized(true); setRefreshKey(k=>k+1); }} className='border-2 p-1 rounded-lg bg-gray-200 hover:bg-gray-300 cursor-pointer'>Finalize (Reset)</button>
-        </div>
+          </div>
           <p className='font-bold'>Total Sales:</p>
           <p className='text-3xl pb-5'>${totalSales.toFixed(2)}</p>
           <p className='font-bold'>Total Transactions:</p>
